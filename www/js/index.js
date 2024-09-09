5b8ed68d46c364a4673ac31021dfdec5887a0d0a
Douglax001 <116027823+Douglax001@users.noreply.github.com>
2024-09-09 11:35:03 -0300

    primeiro commit

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    // Seleciona o menu pelo seletor da classe
+    const menu = document.querySelector('.menu');
+
+    // Alterna a classe "show" no menu
+    menu.classList.toggle('show');
+}
+
+function toggleMenu() {
+    console.log('Menu button clicked!');  // Adicionado para verificar se a função está sendo executada
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show');
+}
+
